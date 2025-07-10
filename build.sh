dcd4d3a0837799e516d290345b290a9926b8d076
misalibaytb <66683443+misalibaytb@users.noreply.github.com>
2025-07-10 14:51:30 +0200

    Update build script to accept repository name as an argument

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
-docker buildx build --platform linux/amd64,linux/arm64 --push -t ghcr.io/${{ github.repository }}:java_8 . -f ./java/8/Dockerfile
-docker buildx build --platform linux/amd64,linux/arm64 --push -t ghcr.io/${{ github.repository }}:java_11 . -f ./java/11/Dockerfile
-docker buildx build --platform linux/amd64,linux/arm64 --push -t ghcr.io/${{ github.repository }}:java_16 . -f ./java/16/Dockerfile
-docker buildx build --platform linux/amd64,linux/arm64 --push -t ghcr.io/${{ github.repository }}:java_17 . -f ./java/17/Dockerfile
-docker buildx build --platform linux/amd64,linux/arm64 --push -t ghcr.io/${{ github.repository }}:java_21 . -f ./java/21/Dockerfile
+repository=$1
+
+
+docker buildx build --platform linux/amd64,linux/arm64 --push -t ghcr.io/$repository:java_8 . -f ./java/8/Dockerfile
+docker buildx build --platform linux/amd64,linux/arm64 --push -t ghcr.io/$repository:java_11 . -f ./java/11/Dockerfile
+docker buildx build --platform linux/amd64,linux/arm64 --push -t ghcr.io/$repository:java_16 . -f ./java/16/Dockerfile
+docker buildx build --platform linux/amd64,linux/arm64 --push -t ghcr.io/$repository:java_17 . -f ./java/17/Dockerfile
+docker buildx build --platform linux/amd64,linux/arm64 --push -t ghcr.io/$repository:java_21 . -f ./java/21/Dockerfile
