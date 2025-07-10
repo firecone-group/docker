ff1c1883ee1343c0bc8db018e16a39c6b8cef2a1
misalibaytb <66683443+misalibaytb@users.noreply.github.com>
2025-07-10 14:53:59 +0200

    Refactor build script to change directory before building Docker images

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 repository=$1
 
-
-docker buildx build --push -t ghcr.io/$repository:java_8 . -f ./java/8/Dockerfile
-docker buildx build --push -t ghcr.io/$repository:java_11 . -f ./java/11/Dockerfile
-docker buildx build --push -t ghcr.io/$repository:java_16 . -f ./java/16/Dockerfile
-docker buildx build --push -t ghcr.io/$repository:java_17 . -f ./java/17/Dockerfile
-docker buildx build --push -t ghcr.io/$repository:java_21 . -f ./java/21/Dockerfile
+cd ./java/8/ && docker buildx build --push -t ghcr.io/$repository:java_8 . && cd - || exit
+cd ./java/11/ && docker buildx build --push -t ghcr.io/$repository:java_11 . && cd - || exit
+cd ./java/16/ && docker buildx build --push -t ghcr.io/$repository:java_16 . && cd - || exit
+cd ./java/17/ && docker buildx build --push -t ghcr.io/$repository:java_17 . && cd - || exit
+cd ./java/21/ && docker buildx build --push -t ghcr.io/$repository:java_21 . && cd - || exit
